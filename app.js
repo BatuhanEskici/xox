81b381b829bbd414f6b4f10c9a70227a4bca6328
Batuhan Eskici <batuhan_eskici@hotmail.com>
2022-01-29 17:00:45 +0300

    checkIsGameOver function added, previos player state added

--- app.js
+++ app.js
@@ -8,9 +8,10 @@ class Game {
         this.rows = document.querySelectorAll(".game-table .row");
         this.columns = document.querySelectorAll(".game-table .column");
         this.current_player_text = document.querySelector("#current_player");
+        this.previous_player = null;
         this.current_player = null;
         this.result = null;
-        this.game_state = [[0,0,0], [0,0,0], [0,0,0]];
+        this.game_state = [[1,2,3], [4,5,6], [7,8,9]];
     }
 
     /**
@@ -58,13 +59,54 @@ class Game {
         this.game_state[row_index][column_index] = this.current_player;
     }
 
+    checkIsGameOver() {
+        const game_state = this.game_state;
+
+        const winner_patterns = {
+            1: [[0,0], [0,1], [0,2]],
+            2: [[1,0], [1,1], [1,2]],
+            3: [[2,0], [2,1], [2,2]],
+            4: [[0,0], [1,0], [2,0]],
+            5: [[0,1], [1,1], [2,1]],
+            6: [[0,2], [1,2], [2,2]],
+            7: [[0,0], [1,1], [2,2]],
+            8: [[0,2], [1,1], [2,0]]
+        }
+
+        for (const key in winner_patterns) {
+            const pattern_row_0 = winner_patterns[key][0][0];
+            const pattern_column_0 = winner_patterns[key][0][1];
+
+            const pattern_row_1 = winner_patterns[key][1][0];
+            const pattern_column_1 = winner_patterns[key][1][1];
+
+            const pattern_row_2 = winner_patterns[key][2][0];
+            const pattern_column_2 = winner_patterns[key][2][1];
+
+            const game_state_item_1 = game_state[pattern_row_0][pattern_column_0];
+            const game_state_item_2 = game_state[pattern_row_1][pattern_column_1];
+            const game_state_item_3 = game_state[pattern_row_2][pattern_column_2];
+
+            if (game_state_item_1 === game_state_item_2 && game_state_item_1  === game_state_item_3) {
+                console.log("Game is over");
+                console.log("The winner is:", this.previous_player);
+                break;
+            } else {
+                console.log("Game isn't over yet");
+            }
+        }
+    }
+
     /**
      * Move game to next move
      */
     moveGameToNextMove() {
+        this.previous_player = this.current_player;
         this.current_player = this.current_player === "X" ? "O" : "X";
 
         this.updateCurrentPlayerText();
+
+        this.checkIsGameOver();
     }
 }
 
