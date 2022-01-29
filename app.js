6bdebd78e6667904a32f0bcb5951ac95fb9c6d07
Batuhan Eskici <batuhan_eskici@hotmail.com>
2022-01-29 14:43:00 +0300

    start method added to game class

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+"use strict";
+
 /**
  * Methods about the game
  */
@@ -5,7 +7,17 @@ class Game {
     constructor() {
         this.rows = document.querySelectorAll(".game-table .row");
         this.columns = document.querySelectorAll(".game-table .column");
+        this.current_player_text = document.querySelector("#current_player");
+        this.current_player = null;
+        this.result = null;
+    }
+
+    start() {
+        this.current_player = "X";
+        this.current_player_text.innerText = `Current player: ${this.current_player}`;
     }
 }
 
 const game = new Game();
+
+game.start();
